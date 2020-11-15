2afcaa9fa9f09fd355d806edc7bc8ea2ab56c55d
TheIcySpark <saacmanjarrez@gmail.com>
2020-11-15 13:29:57 -0600

    Bug fix quick sort

--- src/Algoritmos/quick.js
+++ src/Algoritmos/quick.js
@@ -5,7 +5,6 @@ export function quick(arreglo){
     copia_arreglo = [...arreglo];
     animaciones = [];
     quick_sort(0, copia_arreglo.length - 1);
-    console.log(copia_arreglo);
     return animaciones;
 }
 
@@ -14,20 +13,20 @@ function quick_sort(ini, fin){
     let pivote = fin;
     let posicion_actual = ini;
     while(posicion_actual < pivote){
+        animacion = {};
+        animacion.p1 = pivote;
+        animacion.vp1 = copia_arreglo[pivote];
+        animacion.p2 = posicion_actual;
+        animacion.vp2 = copia_arreglo[posicion_actual];
+        animaciones.push(animacion);
         if (copia_arreglo[posicion_actual] > copia_arreglo[pivote]){
-            animacion = {};
-            animacion.p1 = pivote;
-            animacion.vp1 = copia_arreglo[pivote];
-            animacion.p2 = posicion_actual;
-            animacion.vp2 = copia_arreglo[posicion_actual];
-            animaciones.push(animacion);
-
             animacion = {};
             animacion.p1 = pivote - 1;
             animacion.vp1 = copia_arreglo[posicion_actual];
             animacion.p2 = posicion_actual;
             animacion.vp2 = copia_arreglo[pivote - 1];
             animaciones.push(animacion);
+
             [copia_arreglo[pivote - 1], copia_arreglo[posicion_actual]] = [
                 copia_arreglo[posicion_actual], copia_arreglo[pivote - 1]]
                 
